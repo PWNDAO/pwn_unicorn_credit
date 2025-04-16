7ff5115b35c9d318b98d1ec1e781fddbbf21a753
tohrxyz <tomas.hrib@proton.me>
2025-04-16 17:08:20 +0200

    feat: rework landing page, add fonts, images

--- apps/web/src/pages/Unicorn/UnicornHome.tsx
+++ apps/web/src/pages/Unicorn/UnicornHome.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Text, useMedia } from 'ui/src'
+import { Button, Flex, Text, useMedia, Image } from 'ui/src'
 import { MockSwapComponent } from './MockSwapComponent'
 
 export default function UnicornHome() {
@@ -9,12 +9,12 @@ export default function UnicornHome() {
       minHeight="100vh"
       justifyContent="center"
       alignItems="center"
-      backgroundColor="$background"
+      backgroundColor="transparent"
       p="$spacing24"
       position="relative"
     >
       {/* Background Mock Component */}
-      <Flex
+      {/* <Flex
         position="absolute"
         width="100%"
         height="100%"
@@ -25,31 +25,49 @@ export default function UnicornHome() {
         filter="blur(1.5px)"
       >
         <MockSwapComponent />
-      </Flex>
+      </Flex> */}
 
       {/* Main Content */}
       <Flex
-        backgroundColor="$accent2"
+        // backgroundColor="$accent2"
         p={media.sm ? '$spacing24' : '$spacing40'}
         borderRadius="$rounded16"
-        mt="200px"
         mb="$spacing24"
         alignItems="center"
         width="100%"
-        maxWidth={480}
+        maxWidth={640}
         zIndex={1}
-        shadowColor="$accent1"
-        shadowOffset={{ width: 0, height: 2 }}
-        shadowOpacity={0.25}
-        shadowRadius={3.84}
+        position="relative"
+        // shadowColor="$accent1"
+        // shadowOffset={{ width: 0, height: 2 }}
+        // shadowOpacity={0.25}
+        // shadowRadius={3.84}
+        
       >
+        <Text
+          style={{ 
+            opacity: 0.05,
+            position: 'absolute',
+            top: '50%',
+            left: '40%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 0,
+            fontSize: '500px',
+            lineHeight: 1,
+            userSelect: 'none',
+            pointerEvents: 'none',
+          }}
+        >
+          🦄
+        </Text>
         <Text
           color="$white"
           variant={media.sm ? 'heading2' : 'heading1'}
           mb="$spacing16"
           textAlign="center"
           fontWeight="bold"
-          style={{ textShadow: '0 0 20px rgba(252, 114, 255, 0.2)' }}
+          fontSize={72}
+          style={{ textShadow: '0 0 20px rgba(252, 114, 255, 0.2)', fontFamily: 'Screener' }}
         >
           Coming Soon
         </Text>
@@ -57,33 +75,25 @@ export default function UnicornHome() {
           color="$white"
           variant={media.sm ? 'subheading1' : 'heading3'}
           textAlign="center"
-          style={{ textShadow: '0 0 20px rgba(70, 115, 250, 0.2)' }}
+          style={{ textShadow: '0 0 20px rgba(70, 115, 250, 0.2)', fontFamily: 'Screener' }}
         >
           put your 🦄 unicorn assets to work
         </Text>
       </Flex>
       <Flex
-        backgroundColor="$accent2"
-        py="$spacing8"
-        px="$spacing16"
-        borderRadius="$rounded12"
-        pressStyle={{ opacity: 0.8 }}
-        hoverStyle={{ backgroundColor: '$accent2' }}
-        zIndex={1}
       >
         <Button
-          backgroundColor="$accent2"
-          py="$spacing8"
-          px="$spacing16"
+          backgroundColor="#333333"
+          // py="$spacing8"
+          // px="$spacing16"
           borderRadius="$rounded32"
-          size="medium"
-          hoverStyle={{ backgroundColor: '$accent2' }}
+          // size="medium"
           onPress={() => {
             window.open('https://x.com/pwndao', '_blank')
           }}
         >
-          <Text color="$white" variant="buttonLabel2">
-            Follow for updates
+          <Text color="$white" variant="buttonLabel2" style={{ fontFamily: 'Screener' }}>
+            𝕏 Follow for updates
           </Text>
         </Button>
       </Flex>
