4f566d03928d1a3b9248c4d3e692df363efe83f6
tohrxyz <tomas.hrib@proton.me>
2025-05-13 17:53:47 +0200

    feat: improve styles of LPs (modal, selected)

--- apps/web/src/pages/Unicorn/components/SelectPoolInput.tsx
+++ apps/web/src/pages/Unicorn/components/SelectPoolInput.tsx
@@ -1,6 +1,7 @@
 import { Button, Flex, Text } from 'ui/src'
 import { RotatableChevron } from 'ui/src/components/icons/RotatableChevron'
-import { PoolData, chainName } from 'uniswap/src/components/TokenSelector/lists/TokenSelectorPoolsList'
+import { TokenLogo } from 'uniswap/src/components/CurrencyLogo/TokenLogo'
+import { PoolData, formatNumberWithSuffix } from 'uniswap/src/components/TokenSelector/lists/TokenSelectorPoolsList'
 
 export const SelectPoolInput = ({
   selectedPool,
@@ -53,49 +54,56 @@ export const SelectPoolInput = ({
           </Button>
         </Flex>
       ) : (
-        <Flex width={'$full'} minHeight={100}>
-          <Button
-            width={'$full'}
-            height={'$full'}
-            backgroundColor="$surface1"
-            borderColor="$surface3"
-            borderRadius="$rounded20"
-            borderWidth="$spacing1"
-            px="$spacing16"
-            py="$spacing16"
-            pressStyle={{
-              backgroundColor: 'rgb(35, 33, 34)',
-            }}
-            hoverStyle={{
-              backgroundColor: 'rgb(35, 33, 34)',
-            }}
-            animation="quick"
-            size="large"
-            onPress={handleOpenTokenSelector}
-            justifyContent="unset"
-          >
-            <Flex flexDirection="column" height={'$spacing120'} width="100%" justifyContent="space-between">
-              <Flex flexDirection="row" gap="$spacing12" justifyContent="space-between">
-                <Flex flexDirection="row" gap="$spacing12" justifyContent="space-between">
-                  <Text color="$neutral1" variant="subheading2">
-                    {selectedPool.tokens.token0.symbol} / {selectedPool.tokens.token1.symbol}
-                  </Text>
-                  <Text color="$neutral1" variant="subheading2">
-                    {selectedPool.feeTier / 10000}%
-                  </Text>
-                </Flex>
-                <Text color="$neutral1" variant="subheading2">
-                  ${selectedPool.totalUsdValue.toLocaleString(undefined, { maximumFractionDigits: 2 })}
+        <Flex
+          py="$spacing24"
+          px="$spacing8"
+          onPress={handleOpenTokenSelector}
+          pressStyle={{
+            backgroundColor: 'rgb(35, 33, 34)',
+          }}
+          hoverStyle={{
+            backgroundColor: 'rgb(35, 33, 34)',
+          }}
+          borderRadius={16}
+        >
+          <Flex flexDirection="column" justifyContent="space-between" alignContent="stretch" height={'100%'} mt={-10}>
+            <Flex row gap="$spacing8" alignItems="center">
+              <Flex row gap="$spacing4" alignItems="center">
+                <TokenLogo
+                  size={32}
+                  url={
+                    'https://imgs.search.brave.com/qVfnM06301I6nmM20XJwh7E1dtjKpAU1IA0dllgkXNo/rs:fit:40:40:1:0/g:ce/aHR0cHM6Ly9jb2lu/LWltYWdlcy5jb2lu/Z2Vja28uY29tL2Nv/aW5zL2ltYWdlcy82/MzE5L2xhcmdlL3Vz/ZGMucG5nPzE2OTY1/MDY2OTQ'
+                  }
+                />
+                <Text fontSize={16} color="$neutral1">
+                  {selectedPool.tokens.token0.symbol}
                 </Text>
               </Flex>
-              <Flex flexDirection="row" width="$full" justifyContent="space-between">
-                <Text color="$neutral1" variant="subheading2">
-                  Chain: {chainName(selectedPool.tokens.token0.chainId)}
+              <Text fontSize={16} color="$neutral1">
+                /
+              </Text>
+              <Flex row gap="$spacing4" alignItems="center">
+                <TokenLogo
+                  size={32}
+                  url={
+                    'https://token-repository.dappradar.com/tokens?protocol=ethereum&contract=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&file=logo.png'
+                  }
+                />
+                <Text fontSize={16} color="$neutral1">
+                  {selectedPool.tokens.token1.symbol}
                 </Text>
-                <RotatableChevron color="$neutral1" direction="down" height="$spacing24" />
               </Flex>
+              <RotatableChevron color="$neutral1" direction="down" height="$spacing24" />
             </Flex>
-          </Button>
+          </Flex>
+          <Flex row justifyContent="space-between" alignItems="center">
+            <Text variant="body1" color="$neutral2">
+              ${formatNumberWithSuffix(selectedPool.totalUsdValue)}
+            </Text>
+            <Text variant="body2" color="$neutral2">
+              {Number(selectedPool.feeTier) / 10_000}%
+            </Text>
+          </Flex>
         </Flex>
       )}
     </Flex>
